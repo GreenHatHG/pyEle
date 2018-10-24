83758644f4af7a5b3f0a7169d8398b36e3eb30d1
wyatt17 <1239776759@qq.com>
2018-10-24 10:28:14 +0800

    tmp save

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 def getHTML(url):
     try:
-        url = 'http://kmg.kamigo.cn/link/63MO6OU70MB6K34I'
         r = requests.get(url)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
@@ -13,18 +12,38 @@ def getHTML(url):
 
 def findPrice(html, Pricelist, namelist):
     soup = BeautifulSoup(html, "html.parser")
-    allPrice = soup.find_all(class_="price active")
-    allName = soup.find_all(class_="shoname")
-
-    for price in allPrice:
-        Pricelist.append(price.string)
+    ulTag = soup.find_all("ul")
     
-    for name in allName:
-        namelist.append(name.string)
+    for ul in ulTag:
+        liTag = ul.find_all(class_= 'price active')
+        if(len(liTag) != 0):
+            namelist.append(ul.find_all(class_ = 'shoname'))
+
+    for ul in ulTag:
+        print(ul)
+        
+    '''
+    allPrice = soup.find_all("li",class_="price active")
+    allName = soup.find_all("li", class_="price active").find_previous("li")
+    for price in allName:
+        
+        namelist.append(price.previous_sibling)
+        Pricelist.append(price.string)
+        
+        print(price)
+   '''
+    """
+    for i in namelist:
+        print(i)
+    """
+    """
+    for i in list(range(0, len(namelist))):
+        print(namelist[i] + Pricelist[i])
+    """
 
 def main():
-    Pricelist = ()
-    namelist = ()
+    Pricelist = []
+    namelist = []
     url = 'http://kmg.kamigo.cn/link/63MO6OU70MB6K34I'
     html = getHTML(url)
     findPrice(html, Pricelist, namelist)
