45c3099eead7d367342a6e943c51a16561e3a43b
GreenHatHG <1239776759@qq.com>
2018-10-24 11:18:46 +0800

    可以查找一个网站的商品，价格，库存

--- test.py
+++ test.py
@@ -10,43 +10,28 @@ def getHTML(url):
     except:
         return ""
 
-def findPrice(html, Pricelist, namelist):
+def findPrice(html, Pricelist, Namelist, number):
     soup = BeautifulSoup(html, "html.parser")
-    ulTag = soup.find_all("ul")
+    ulTag = soup.find_all("ul", class_ = "ord_listall qs-cleaar")
     
     for ul in ulTag:
-        liTag = ul.find_all(class_= 'price active')
-        if(len(liTag) != 0):
-            namelist.append(ul.find_all(class_ = 'shoname'))
+        Namelist.append(ul.find(class_ = "shoname").string)
+        Pricelist.append(ul.find(class_ = "price active").string)
+        number.append(ul.find(class_ = "kc").string)
 
-    for ul in ulTag:
-        print(ul)
-        
-    '''
-    allPrice = soup.find_all("li",class_="price active")
-    allName = soup.find_all("li", class_="price active").find_previous("li")
-    for price in allName:
-        
-        namelist.append(price.previous_sibling)
-        Pricelist.append(price.string)
-        
-        print(price)
-   '''
-    """
-    for i in namelist:
-        print(i)
-    """
-    """
-    for i in list(range(0, len(namelist))):
-        print(namelist[i] + Pricelist[i])
-    """
+    Len = 30
+    print('|' + '商品'.ljust(Len) + '|' + '价格'.ljust(Len) + '|' + '库存'.ljust(Len)) 
+    for i in list(range(0, len(Namelist))):
+        print(Namelist[i].ljust(Len) +  Pricelist[i].ljust(Len) + number[i].ljust(Len))
+
 
 def main():
     Pricelist = []
     namelist = []
+    number = []
     url = 'http://kmg.kamigo.cn/link/63MO6OU70MB6K34I'
     html = getHTML(url)
-    findPrice(html, Pricelist, namelist)
+    findPrice(html, Pricelist, namelist, number)
 
 main()
 
