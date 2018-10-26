4a4aa0281f436c9edde25e869e7282d1131af036
wyatt17 <1239776759@qq.com>
2018-10-26 13:34:06 +0800

    add TODO

--- test.py
+++ test.py
@@ -19,19 +19,28 @@ def findPrice(html, Pricelist, Namelist, number):
         Pricelist.append(ul.find(class_ = "price active").string)
         number.append(ul.find(class_ = "kc").string)
 
+def prin(Pricelist, Namelist, number):
     Len = 30
     print('|' + '商品'.ljust(Len) + '|' + '价格'.ljust(Len) + '|' + '库存'.ljust(Len)) 
     for i in list(range(0, len(Namelist))):
         print(Namelist[i].ljust(Len) +  Pricelist[i].ljust(Len) + number[i].ljust(Len))
 
-
 def main():
     Pricelist = []
     namelist = []
     number = []
-    url = 'http://kmg.kamigo.cn/link/63MO6OU70MB6K34I'
-    html = getHTML(url)
-    findPrice(html, Pricelist, namelist, number)
-
+    #url = 'http://kmg.kamigo.cn/link/63MO6OU70MB6K34I'
+   # kamigoUrl = ['http://t.cn/EZXjEzS', 'http://t.cn/EZXHcl3']
+    kamigoUrl = ['http://t.cn/EZXE7KV',]
+    for url in kamigoUrl:
+        html = getHTML(url)
+        findPrice(html, Pricelist, namelist, number)
+    prin(Pricelist, namelist, number)
 main()
 
+'''
+TODO:
+    read url from file and classify
+    sort the Pricelist and print
+    print with a websites
+'''
